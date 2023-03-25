dac0befa96c0799329b579c4c6f8ef774e4dc7da
NTQ <NTQ@DESKTOP-D2HG5SS>
2023-03-25 21:55:00 +0700

    Add Layout Admin

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,6 +26,20 @@ namespace CuoiKy.Controllers
             else
                 return View(all_SanPham.ToPagedList(pageNum, pageSize));
         }
+        public ActionResult IndexAdmin(int? page, string SearchString)
+        {
+            int pageSize = 8;
+            int pageNum = page ?? 1;
+            var all_SanPham = context.SanPhams.OrderBy(s => s.TenSanPham);
+            var all_SanPhamTK = context.SanPhams.OrderBy(m => m.TenSanPham).Where(sp => sp.TenSanPham.ToUpper().Contains(SearchString.ToUpper()));
+            page = 1;
+            if (SearchString == null || SearchString == "")
+                return View(all_SanPham.ToPagedList(pageNum, pageSize));
+            else if (all_SanPhamTK != null)
+                return View(all_SanPhamTK.ToPagedList(pageNum, pageSize));
+            else
+                return View(all_SanPham.ToPagedList(pageNum, pageSize));
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
