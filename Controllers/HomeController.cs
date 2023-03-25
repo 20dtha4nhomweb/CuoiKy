4613be97e49eeb0794a2bf402e4a11ca7132b3de
lightofthemoon <nguyentrongquy612@gmail.com>
2023-03-25 14:16:28 +0700

    custom lại layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,19 +14,18 @@ namespace CuoiKy.Controllers
         DataMyPhamContext context = new DataMyPhamContext();
         public ActionResult Index(int? page, string SearchString)
         {
-            int pageSize = 9;
+            int pageSize = 8;
             int pageNum = page ?? 1;
             var all_SanPham = context.SanPhams.OrderBy(s => s.TenSanPham);
             var all_SanPhamTK = context.SanPhams.OrderBy(m => m.TenSanPham).Where(sp => sp.TenSanPham.ToUpper().Contains(SearchString.ToUpper()));
             page = 1;
-            if (all_SanPhamTK.ToString() == "")
+            if (SearchString == null || SearchString == "")
+                return View(all_SanPham.ToPagedList(pageNum, pageSize));
+            else if(all_SanPhamTK != null)
                 return View(all_SanPhamTK.ToPagedList(pageNum, pageSize));
             else
                 return View(all_SanPham.ToPagedList(pageNum, pageSize));
         }
-
-
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -40,60 +39,5 @@ namespace CuoiKy.Controllers
 
             return View();
         }
-
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Resigter(TaiKhoan tk)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var check = context.TaiKhoans.FirstOrDefault(a => a.TenDangNhap == a.TenDangNhap);
-        //        if (check != null)
-        //        {
-
-        //            context.Configuration.ValidateOnSaveEnabled = false;
-        //            context.TaiKhoans.Add(tk);
-        //            context.SaveChanges();
-        //            return RedirectToAction("Index");
-        //        }
-        //        else
-        //        {
-        //            ViewBag.error = "Tên tài khoản đã tồn tại";
-        //            return View();
-        //        }
-        //    }
-        //    return View();
-        //}
-
-        //Login chưa được
-        //public ActionResult Login()
-        //{
-        //    return View();
-        //}
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Login(string tenTK, string matKhau)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var data = context.TaiKhoans.Where(s=>s.TenDangNhap.Equals(tenTK)&&s.MatKhau.Equals(matKhau));
-        //        if(data.Count() > 0)
-        //        {
-        //            Session["idUser"] = data.FirstOrDefault().TenDangNhap;
-        //            return RedirectToAction("Index");
-        //        }
-        //        else
-        //        {
-        //            ViewBag.error = "Tài khoản hoặc mật khẩu không chính xác!";
-        //            return RedirectToAction("Login");
-        //        }
-        //    }
-        //    return View();
-        //}
-        
-
-
-
-
     }
 }
